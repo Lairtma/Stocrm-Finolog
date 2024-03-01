8d6fc2f1fbb915f5c683c24d250b1e976b1f3b68
pashakai <kayatskypavel@gmail.com>
2024-03-01 21:21:24 +0300

    added func into fing.py added functions(works-parts-value) changed func comparing_deals

--- new_ord_and_contr.py
+++ new_ord_and_contr.py
@@ -7,39 +7,51 @@ def filling_order(type, contr_var, deal_id):
     new_order = fin.create_order(type)  # создаем новый заказ в finolog
     get_info_last_order = fin.get_last_order()  # получаем данные последнего созданного заказа
     last_order_id = get_info_last_order["id"]  # получаем id последнего созданного заказа из данных
-    fin.change_order_by_id(last_order_id, buyer_id=contr_var, description = deal_description)  # изменяем заказ по id добавляя нужные параметры
+    fin.change_order_by_id(last_order_id, buyer_id=contr_var, description = deal_description, status_id=60244)  # изменяем заказ по id добавляя нужные параметры
 
 def comparing_deals(sto_id):
     orders_list = fin.get_all_orders()
     for i in orders_list:
-        desc = i['description']
-        if desc != '' and desc is not None:
-            deal_id = int(''.join(i for i in desc if i.isdigit()))
-            if deal_id == int(sto_id):
-                return deal_id
+        if i['description'] == f"По сделке: {sto_id}":
+            return i['id']
 
-def order_creation_with_contr_data(id,type):
+def order_creation_with_contr_data(deal_id):
     type = "out"
-    deal_data = sto.get_leed_by_id(id) #получаем info по определнной сделке stocrm с помощью её id
-    print(deal_data)
+    deal_data = sto.get_leed_by_id(deal_id) #получаем info по определнной сделке stocrm с помощью её id
     contact_phone_by_leed_id = str(deal_data['RESPONSE']['DATA'][0]['CONTACT_PROPERTY_PHONE']) #получаем номер телефона из сделки storcrm
     contr_list_7 = fin.get_contractor_by_phone('7' + contact_phone_by_leed_id[1:])
     contr_list_8 = fin.get_contractor_by_phone('8' + contact_phone_by_leed_id[1:])
-
     if not contr_list_7 and not contr_list_8: #если такого контрагента не сущемтвует
         fin.create_contractor(name=deal_data['RESPONSE']['DATA'][0]['CONTACT_TITLE'],
-                              phone=deal_data['RESPONSE']['DATA'][0]['CONTACT_PROPERTY_PHONE'], desc=id)
+                              phone=deal_data['RESPONSE']['DATA'][0]['CONTACT_PROPERTY_PHONE'], desc=deal_id)
         contr_id = fin.get_contractor_by_phone(deal_data['RESPONSE']['DATA'][0]['CONTACT_PROPERTY_PHONE'])
-        print(contr_id)
-        filling_order(type, contr_id, id)
+        filling_order(type, contr_id, deal_id)
     else: #такой контрагент уже существует
         contr_list_universal = contr_list_7 if not contr_list_8 else contr_list_8
         contr_id = contr_list_universal[0]['id']
-        print(contr_list_universal)
-        filling_order(type,contr_id, id)
+        filling_order(type, contr_id, deal_id)
+def works_and_parts_value(deal_id):
+    fin_order_id = comparing_deals(deal_id) #данные о заказе из fin связанный с sto
+    order_data = fin.get_order_by_id(fin_order_id)
+    deal_data = sto.get_leed_by_id(deal_id) #данные о сделке stocrm
+    orders_sum = float(deal_data['RESPONSE']['DATA'][0]['ORDERS_SUM']) #стоимость запчастей в заказе
+    products_sum = (float(deal_data['RESPONSE']['DATA'][0]['OFFER_SUM']) - orders_sum) #стоимость работ в заказе
+    package_id = order_data['package_id']
+    fin.add_package_item_by_id(package_id, 115857, count=1, price=orders_sum) #добавление стоимости запчастей в заказ
+    fin.add_package_item_by_id(package_id, 153056, count=1, price=products_sum) #добавление стоимости работ в заказ
+    fin.update_package_by_id(package_id)
 
+"""
+("status_id = {None:'new', 60244:'in work', 
+60245: 'waiting payment, 60246:'closed', 60247:'declined'}")
+"""
+"""
+id item запчасти: 115857
+id item работа сервиса: 153056
+"""
+#1102293
 sto = Stocrm("13581_bf2b8cec383601bad6765d4b61240dbd", "v8-centr")
 fin = Finolog("hepV7NAnFgAshnDd90adec7e4d95088359e869f3e4f89e08riNSzPykUqS6fKWN", "43768")
 
-(order_creation_with_contr_data(10715, "out"))
-print(comparing_deals(10715))
+
+
