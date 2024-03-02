f11ad32e914a8870c26547b59a9d4e8dbb377e89
pashakai <kayatskypavel@gmail.com>
2024-03-02 16:54:41 +0300

    added func into fing.py changed func(works-parts-value) changed func comparing_deals

--- fing.py
+++ fing.py
@@ -202,13 +202,21 @@ class Finolog:
         url = f"https://api.finolog.ru/v1/biz/{self.biz_id}/orders/order"
         params = {
             "api_token": self.api_token,
-            "type": type
+            "type": type,
         }
         req = requests.post(url, params=params)
         json_acceptable_string = req.text.replace("'", "\"")
         lst = json.loads(json_acceptable_string)
         return lst
-
+    def create_shipment(self,id):
+        url = f"https://api.finolog.ru/v1/biz/{self.biz_id}/orders/order/{id}/shipments"
+        params = {
+            "api_token": self.api_token,
+        }
+        req = requests.post(url, params=params)
+        json_acceptable_string = req.text.replace("'", "\"")
+        lst = json.loads(json_acceptable_string)
+        return lst
     def change_order_by_id(self, id,  *args, **kwargs):
         url = f"https://api.finolog.ru/v1/biz/{self.biz_id}/orders/order/{id}"
         params = {
@@ -231,13 +239,14 @@ class Finolog:
         lst = json.loads(json_acceptable_string)
         return lst
 
-    def add_package_item_by_id(self, package_id, item_id, count, price):
+    def add_package_item_by_id(self, package_id, item_id, count, price,project_id):
         url = f"https://api.finolog.ru/v1/biz/{self.biz_id}/orders/package/{package_id}/item"
         params = {
             "api_token": self.api_token,
             "count":count,
             "price":price,
-            "item_id":item_id
+            "item_id":item_id,
+            "project_id":project_id
         }
         req = requests.post(url, params=params)
         json_acceptable_string = req.text.replace("'", "\"")
@@ -264,13 +273,13 @@ class Finolog:
         lst = json.loads(json_acceptable_string)
         return lst
 
-    def change_package_item_by_id(self, package_id, package_item_id, count, price):
+    def change_package_item_by_id(self, package_id, package_item_id, count, price,project_id):
         url = f"https://api.finolog.ru/v1/biz/{self.biz_id}/orders/package/{package_id}/item/{package_item_id}"
         params = {
             "api_token": self.api_token,
             "count": count,
             "price": price,
-            "item_id": package_item_id
+            "project_id":project_id
         }
         req = requests.put(url, params=params)
         json_acceptable_string = req.text.replace("'", "\"")
@@ -282,4 +291,3 @@ if __name__ == "__main__":
     fin = Finolog("hepV7NAnFgAshnDd90adec7e4d95088359e869f3e4f89e08riNSzPykUqS6fKWN", "43768")
     k = fin.get_all_contractors()
     print(k)
-
--- new_ord_and_contr.py
+++ new_ord_and_contr.py
@@ -37,14 +37,24 @@ def works_and_parts_value(deal_id):
     orders_sum = float(deal_data['RESPONSE']['DATA'][0]['ORDERS_SUM']) #стоимость запчастей в заказе
     products_sum = (float(deal_data['RESPONSE']['DATA'][0]['OFFER_SUM']) - orders_sum) #стоимость работ в заказе
     package_id = order_data['package_id']
-    if order_data['package']['items'] and (order_data['package']['items'][0]['item_name'] != 'Запчасти' and order_data['package']['items'][0]['item_name'] != 'Работа сервиса'):
+    if deal_data['RESPONSE']['DATA'][0]["OFFER_CUSTOMER_NAME"] == "V8.CENTER":
+        project_id = 358087
+    elif deal_data['RESPONSE']['DATA'][0]["OFFER_CUSTOMER_NAME"] == "V8.CENTER Кузовной":
+        project_id = 353644
+    else:
+        project_id = 345944
+    fin.change_order_by_id(fin_order_id, project_id=project_id)
+    if not order_data['package']['items']:
         fin.add_package_item_by_id(package_id, 115857, count=1, price=orders_sum) #добавление стоимости запчастей в заказ
         fin.add_package_item_by_id(package_id, 153056, count=1, price=products_sum) #добавление стоимости работ в заказ
     else:
-        fin.change_package_item_by_id(package_id, 115857, count=1, price=orders_sum)
-        fin.change_package_item_by_id(package_id, 153056, count=1, price=products_sum)
+        check = order_data['package']['items'][0]['item_id']
+        if check == 115857:
+            package_item_id = order_data['package']['items'][0]['id']
+            fin.change_package_item_by_id(package_id, package_item_id, count=1, price=orders_sum,project_id = project_id)
+            package_item_id = order_data['package']['items'][1]['id']
+            fin.change_package_item_by_id(package_id, package_item_id, count=1, price=products_sum, project_id =project_id )
     fin.update_package_by_id(package_id)
-
 """
 ("status_id = {None:'new', 60244:'in work', 
 60245: 'waiting payment, 60246:'closed', 60247:'declined'}")
@@ -56,4 +66,7 @@ id item работа сервиса: 153056
 sto = Stocrm("13581_bf2b8cec383601bad6765d4b61240dbd", "v8-centr")
 fin = Finolog("hepV7NAnFgAshnDd90adec7e4d95088359e869f3e4f89e08riNSzPykUqS6fKWN", "43768")
 
+
 print(fin.get_order_by_id(410356))
+fin.create_shipment(410356)
+#works_and_parts_value(10892)
